ed3b8c628badf06daac38390b2cef3686c4d1784
NicqChen <nicq.chen@outlook.com>
2016-06-11 22:10:42 +0800

    HackerRanker

--- src/Solution.java
+++ src/Solution.java
@@ -1,89 +1,30 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
 import java.math.*;
-import java.util.regex.*;
 
-public class Solution {
+class Solution {
+    /**
+     * @param A: An integer array.
+     * @param B: An integer array.
+     * @return: Cosine similarity.
+     */
+    public double cosineSimilarity(int[] A, int[] B) {
+        // write your code here
+        long sumA = 1l, sumB = 1l, sumC =1l;
 
-    static class Edge {
-        int v, w, c;
-
-        public Edge(int v, int w, int c) {
-            this.v = v;
-            this.w = w;
-            this.c = c;
+        for(int i=0; i<A.length; i++) {
+            sumA += (A[i] * A[i]);
+            sumB += (B[i] * B[i]);
+            sumC += (A[i] * B[i]);
         }
 
-        public int either() {return v;}
+        if(sumA == 0 || sumB == 0) return 2.0000;
 
-        public int other(int verx) {
-            if( verx == v) return w;
-            if (verx == w) return v;
-            return -1;
-        }
+        double d = Math.sqrt(sumA) * Math.sqrt(sumB);
+
+        return new BigDecimal(sumC/d).setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int i = 0; i < T; i++) {
-            int v = sc.nextInt();
-            int e = sc.nextInt();
-
-            int maxV = v + 1;
-            LinkedList<Edge> [] graph = new LinkedList[maxV];
-            boolean[] inTree = new boolean[maxV];
-            int[] distTo = new int[maxV];
-            Arrays.fill(distTo, Integer.MAX_VALUE);
-
-            for (int j = 0; j < maxV; j++) {
-                graph[j] = new LinkedList<Edge>();
-            }
-
-            for (int j = 0; j < e; j++) {
-                int f = sc.nextInt();
-                int t = sc.nextInt();
-                int w = sc.nextInt();
-
-                Edge edge = new Edge(f, t, w);
-                graph[f].add(edge);
-                graph[t].add(edge);
-
-
-            }
-
-            int s = sc.nextInt();
-            int start = s;
-            distTo[s] = 0;
-
-            while (!inTree[s]) {
-                inTree[s] = true;
-                for(Edge edge: graph[s]) {
-                    if(distTo[edge.other(s)] > (distTo[s] + edge.c)) {
-                        distTo[edge.other(s)] = (distTo[s] + edge.c);
-                    }
-                }
-
-                s = 1;
-                int dist = Integer.MAX_VALUE;
-                for (int j = 0; j < maxV; j++) {
-                    if(!inTree[j] && dist > distTo[j]) {
-                        s = j;
-                        dist = distTo[j];
-                    }
-                }
-            }
-
-            for (int j = 1; j < maxV; j++) {
-                if (j == start) continue;
-                if (distTo[j] == Integer.MAX_VALUE) System.out.print("-1 ");
-                else System.out.print(distTo[j]+" ");
-            }
-
-            System.out.println();
-
-        }
-
+        System.out.println(new Solution().cosineSimilarity(new int[]{1,4,0}, new int[]{1,2,3}));
     }
 }
